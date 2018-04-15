e40e8a9f461a2821cfb24e4d690c74046f7f6cdd
srijith <30682653+srijithsrk@users.noreply.github.com>
2018-04-15 21:47:43 +0530

    updated
    the problem is resolved

--- deadlock_detect.c
+++ deadlock_detect.c
@@ -54,86 +54,78 @@ printf("\nThe Maximum Claim Table:\n");
 for (i = 0; i < processes; i++) 
 {
 for (j = 0; j < resources; j++) 
-        {
-                printf("\t%d", maximum_claim[i][j]);
-            }
-            printf("\n");
-        }
- 
-        for (i = 0; i < processes; i++) 
-    {
-            for (j = 0; j < resources; j++) 
-        {
-                    allocation[j] += current[i][j];
-            }
-        }
- 
-        printf("\nAllocated resources:");
-        for (i = 0; i < resources; i++) 
-    {
-            printf("\t%d", allocation[i]);
-        }
- 
-        for (i = 0; i < resources; i++) 
-    {
-            available[i] = maxres[i] - allocation[i];
-    }
- 
-        printf("\nAvailable resources:");
-        for (i = 0; i < resources; i++) 
-    {
-            printf("\t%d", available[i]);
-        }
-        printf("\n");
- 
-        while (counter != 0) 
-    {
-            safe = 0;
-            for (i = 0; i < processes; i++) 
-        {
-                    if (running[i]) 
-            {
-                        exec = 1;
-                        for (j = 0; j < resources; j++) 
-                {
-                                if (maximum_claim[i][j] - current[i][j] > available[j]) 
-                    {
-                                    exec = 0;
-                                    break;
-                                }
-                        }
-                        if (exec) 
-                {
-                                printf("\nProcess%d is executing\n", i + 1);
-                                running[i] = 0;
-                                counter--;
-                                safe = 1;
- 
-                                for (j = 0; j < resources; j++) 
-                    {
-                                    available[j] += current[i][j];
-                                }
-                            break;
-                        }
-                    }
-            }
-            if (!safe) 
-        {
-                    printf("\nThe processes are in unsafe state.\n");
-                    break;
-            } 
-        else 
-        {
-                    printf("\nThe process is in safe state");
-                    printf("\nAvailable vector:");
- 
-                    for (i = 0; i < resources; i++) 
-            {
-                        printf("\t%d", available[i]);
-                    }
- 
-                printf("\n");
-            }
-        }
-        return 0;
+{
+printf("\t%d", maximum_claim[i][j]);
+}
+printf("\n");
+}
+for (i = 0; i < processes; i++) 
+{
+for (j = 0; j < resources; j++) 
+{
+allocation[j] += current[i][j];
+}
+}
+printf("\nAllocated resources:");
+for (i = 0; i < resources; i++) 
+{
+printf("\t%d", allocation[i]);
+}
+for (i = 0; i < resources; i++) 
+{
+available[i] = maxres[i] - allocation[i];
+}
+printf("\nAvailable resources:");
+for (i = 0; i < resources; i++) 
+{
+printf("\t%d", available[i]);
+}
+printf("\n");
+while (counter != 0) 
+{
+safe = 0;
+for (i = 0; i < processes; i++) 
+{
+if (running[i]) 
+{
+exec = 1;
+for (j = 0; j < resources; j++) 
+{
+if (maximum_claim[i][j] - current[i][j] > available[j]) 
+{
+exec = 0;
+break;
+}
+}
+if (exec) 
+{
+printf("\nProcess%d is executing\n", i + 1);
+running[i] = 0;
+counter--;
+safe = 1;
+for (j = 0; j < resources; j++) 
+{
+available[j] += current[i][j];
+}
+break;
+}
+}
+}
+if (!safe) 
+{
+printf("\nThe processes are in unsafe state.\n");
+break;
+} 
+else 
+{
+printf("\nThe process is in safe state");
+printf("\nAvailable vector:");
+for (i = 0; i < resources; i++) 
+{
+printf("\t%d", available[i]);
+}
+printf("\n");
+}
+}
+return 0;
 }
